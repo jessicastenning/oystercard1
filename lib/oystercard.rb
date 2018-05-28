371a623a89d63ab617dba03f30e372a0d6a93c29
jessica stenning <jessicastenning@hotmail.co.uk>
2018-05-28 21:35:26 +0100

    adds top-up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(cash)
+    @balance =+ cash 
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'should have a default balance of zero' do
-    expect(subject.balance).to eq 0
+
+  describe '#balance' do
+    it 'should have a default balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'should allow the user to top up the card balance' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
   end
 end
